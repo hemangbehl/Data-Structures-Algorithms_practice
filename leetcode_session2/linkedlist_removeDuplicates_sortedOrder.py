ef612da24ef0b60e9c9fa05d6963e197b3e125e0
Hemang Behl <44247931+hemangbehl@users.noreply.github.com>
2019-11-01 18:57:58 -0700

    code to remove duplicates from a  linked list _sorted

---
+++ leetcode_session2/linkedlist_removeDuplicates_sortedOrder.py
@@ -0,0 +1,76 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def printList(self):
+        curr = self.head
+        print ("#start#")
+        while curr!=None: #while loop until it reaches the end 
+            print (curr.data)
+            curr = curr.next #go to next node
+
+    def insertHead(self,ele):
+        new = Node(ele)
+        new.next = self.head
+        self.head = new
+    
+    def insertEnd(self,ele):
+        new = Node(ele)
+        new.next = None
+        curr = self.head
+        #check id list is empty
+        if self.head == None:
+            self.head = new
+            print("Inserted at head as list was empty")
+            return
+        else:
+            while (curr!=None):
+                prev = curr
+                curr = curr.next
+            
+            #at end of loop, curr is none and prev has some value
+            prev.next = new
+            new.next = curr
+            print ("Inserted at the end")
+            return
+
+    def removeDuplicatesSorted(self):
+        curr = self.head
+        if curr == None: return
+        temp = 0
+        while curr!=None and curr.next!=None:
+            if curr.data == curr.next.data:
+                #delete next node
+                temp = curr.next.next
+                curr.next.next = None
+                curr.next = temp
+            else: #advance the list
+                curr = curr.next
+        
+
+
+
+#code
+ll = LinkedList()
+ll.head = Node(1)
+second = Node(2)
+third = Node(3)
+
+#linking lists
+ll.head.next = second
+second.next = third
+
+#print
+ll.insertEnd(4)
+#ll.printList()
+ll.insertEnd(5)
+
+ll.insertEnd(5)
+ll.printList()
+ll.removeDuplicatesSorted()
+ll.printList()
