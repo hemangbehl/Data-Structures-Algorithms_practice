e56ad96665b95d14cbbff2ce63c2ded8eac990fc
Hemang Behl <44247931+hemangbehl@users.noreply.github.com>
2019-12-25 20:09:56 -0800

    stack q17 recurssive solution O(2^n - 1)

---
+++ leetcode_session2/stack_q17_Tower_of_Hanoi_recurssive.py
@@ -0,0 +1,19 @@
+'''
+to move all disks from A to C with B as aux
+
+1. move n-1 disks from A to B using C
+2. move remaining 1 disk from A to C
+3. move n-1 disks from B to C using A
+
+TC: O(2^n - 1)
+
+'''
+def TOH ( n, A, C, B):
+    #takes no. of disks, src, dest, using aux
+    if n>0:
+        TOH(n-1, A, B, C) #A to B using C
+        print("Move disk#",n," from",A," to", C)
+        TOH(n-1, B, C, A) #B to C using A
+
+#driver code
+TOH(3, 'A', 'C', 'B')
