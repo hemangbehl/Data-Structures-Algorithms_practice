e1a02b199fc81215a0846f75c6a12582e01ad9db
Hemang Behl <44247931+hemangbehl@users.noreply.github.com>
2020-01-02 19:19:43 -0800

    added file

--- python_textbook_questions/binary_tree_q27_find_ancestors.py
+++ python_textbook_questions/binary_tree_q27_find_ancestors.py
@@ -1,3 +1,6 @@
+''' works only when no duplicate nodes exist.
+Assumes unique nodes. Returns false if not found.
+'''
 class Node:
     def __init__(self, data):
         self.data = data
@@ -26,28 +29,53 @@ class BinaryTree:
             if curr.right != None:
                 queue.append(curr.right)
     
-# Function to find height of a tree  
-def checkSum(curr, n):
-    if curr == None:
-        return False
-    rem = n - curr.data
-    if rem == 0: #found sum
-        return True
-    elif rem < 0: #sum of path is too large
+# Function 
+def findAncestors( root, n1, n2):
+    if root == None:
+        return None
+    ans = [None] #mutable list stores ancestor
+
+    #call recursive func to set ans[0]
+    ancestor(root, n1, n2, ans)
+
+    return ans[0]
+
+def ancestor(curr, n1, n2, ans):
+    if curr == None or ans[0] != None:
         return False
     
-    #else: #keep checking
-    left = checkSum(curr.left, rem)
-    right = checkSum(curr.right, rem)
-    return (left or right) #return OR of left, right
+    mid = False #default
+    if curr.data == n1 or curr.data == n2:
+        mid = True
+    left = ancestor(curr.left, n1, n2, ans)
+
+    if (mid + left) == 2:
+        #found ancestor as mid and left are true
+        ans[0] = curr.data
+        return False
     
+    right = ancestor(curr.right, n1, n2, ans)
+
+    if (mid + left + right) >= 2:
+        #found twice
+        ans[0] = curr.data
+        return False
+    
+    return (mid or left or right) #returns True if present, False if absent
+
+
+
 ##driver code
 bt = BinaryTree()
 bt.root = Node(1)
 bt.root.left = Node(2)
-bt.root.left.left = Node(6)
+bt.root.left.left = Node(4)
+bt.root.left.right = Node(5)
 bt.root.right = Node(3)
-bt.root.right.left = Node(4)
-bt.root.right.right = Node(5)
+bt.root.right.left = Node(6)
+bt.root.right.right = Node(7)
 bt.print()
-print("sum of exists:", checkSum(bt.root, 5) )
+print("Ancestor= {}".format(findAncestors(bt.root, 5, 6)) )
+print("Ancestor= {}".format(findAncestors(bt.root, 4,5)) )
+print("Ancestor= {}".format(findAncestors(bt.root, 3,3)) )
+print("Ancestor= {}".format(findAncestors(bt.root, 5, 10)) )
