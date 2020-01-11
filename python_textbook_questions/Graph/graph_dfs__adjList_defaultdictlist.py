0b521315d156abf428ff39ab6b586080f07de2bd
Hemang Behl <44247931+hemangbehl@users.noreply.github.com>
2020-01-10 17:05:14 -0800

    .

--- python_textbook_questions/Graph/graph_dfs__adjList_defaultdictlist.py
+++ python_textbook_questions/Graph/graph_dfs__adjList_defaultdictlist.py
@@ -16,8 +16,23 @@ class Graph:
                 print(j, end='-> ')
             print("")
 
+    def DFS(self, v):
+        stack = list()
+        stack.append(v)
+        visited = set()
+        visited.add(v)
 
+        while stack:
+            v = stack.pop()
+            print(v, end=' ')
+            
+            for w in self.graph[v]:
+                if w not in visited:
+                    visited.add(w)
+                    stack.append(w)
+        print("")
 
+#driver code
 g = Graph(5)
 g.add_edge(1, 0)
 g.add_edge(2, 1)
@@ -25,4 +40,5 @@ g.add_edge(4, 0)
 g.add_edge(3, 4)
 g.add_edge(0, 3)
 g.add_edge(0, 2)
+g.DFS(0)
 g.printAllEdges()
