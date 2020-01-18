c061839c3584ffc03cb881e3c20c97405b9b5080
Hemang Behl <44247931+hemangbehl@users.noreply.github.com>
2020-01-17 20:01:08 -0800

    edited

--- python_textbook_questions/quicksort_first_ele_pivot.py
+++ python_textbook_questions/quicksort_first_ele_pivot.py
@@ -10,29 +10,29 @@ def quicksort(arr, low, high):
 
 def partition(arr, low, high):
     #set pivot as last ele, high
-    pivot = arr[high]
+    pivot = arr[low]
     #set index i for smaller element as low-1
-    i = low - 1
+    i = low + 1
     #print("partition", )
-    for j in range(low, high): # j = low, j < high
+    for j in range(low+1 , high): # j = low, j < high
         #check if curr ele 'j' is < pivot
         #print("curr:{}, pivot:{}".format(arr[j], pivot ) )
         if arr[j] < pivot:
             #inc 'i' and swap i and j elements
-            i = i + 1
             arr[i], arr[j] = arr[j], arr[i]
+            i = i + 1
             #print("swapped", arr)
     #end of for
 
     #inc 'i' and swap pivot
-    i = i + 1
-    arr[i], arr[high] = arr[high], arr[i]
+    i = i - 1
+    arr[i], arr[low] = arr[low], arr[i]
 
     return i #this is the partition index
 
 #driver code
 if __name__ == "__main__":
-    arr = [1,8,2,4,3,565,87,12]
+    arr = [1,8,2,4,9,41,2,77]
     print(arr)
     quicksort(arr, 0, len(arr)-1 )
     print("after sorting", arr)
