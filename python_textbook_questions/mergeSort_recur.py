379eff75859b9462badb74eb557a8fb3a0b8b44f
Hemang Behl <44247931+hemangbehl@users.noreply.github.com>
2020-01-21 17:46:14 -0800

    mergesort

---
+++ python_textbook_questions/mergeSort_recur.py
@@ -0,0 +1,44 @@
+def mergeSort(myList):
+    if len(myList) > 1: #atleast 2 elements
+        mid = len(myList) // 2
+        left = myList[:mid]  #0 to mid-1
+        right = myList[mid:] #mid-1 to end
+
+        # Recursive call on each half
+        mergeSort(left)
+        mergeSort(right)
+
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                # The value from the left half has been used
+                myList[k] = left[i]
+                # Move the iterator forward
+                i += 1
+            else:
+                myList[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            myList[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            myList[k]=right[j]
+            j += 1
+            k += 1
+
+myList = [54,26,93,17,77,31,44,55,20]
+print("before sorting", myList)
+mergeSort(myList)
+print(myList)
