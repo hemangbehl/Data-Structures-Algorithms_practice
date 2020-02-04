584ef974e70ed80afebca548e07de713614da4e1
Hemang Behl <44247931+hemangbehl@users.noreply.github.com>
2020-02-03 19:37:41 -0800

    temp

--- python_textbook_questions/temp1.py
+++ python_textbook_questions/temp1.py
@@ -1,19 +1,37 @@
-def countWays( arr,  n):
-    prevOneIdx = -1
-    numWays = 0
-    #for ( i = 0; i < n; ++i)
-    for i in range(0, n):
-        if arr[i] == '1':
-            if prevOneIdx != -1:
-                numOnes = i - prevOneIdx
-                numWays *= numOnes
-            else:
-                # there is at least one in the input
-                numWays = 1
-            prevOneIdx = i
-    return numWays
+rotset = set()
+i = 0
+j = 0
+rotset.add( (i, j) )
+rotset.add( (i, j) )
+rotset.add( (i+1, j) )
+rotset.add( (i, j+1) )
 
-#driver code
+set2 =  (i,j+2)
+rotset.add(set2)
 
-arr = '10101'
-print ("num ways:", countWays(arr, len(arr) ) )
+print( rotset )
+
+for ele in rotset:
+    a = ele[0]
+    b = ele[1]
+    print(a, b)
+
+# def countWays( arr,  n):
+#     prevOneIdx = -1
+#     numWays = 0
+#     #for ( i = 0; i < n; ++i)
+#     for i in range(0, n):
+#         if arr[i] == '1':
+#             if prevOneIdx != -1:
+#                 numOnes = i - prevOneIdx
+#                 numWays *= numOnes
+#             else:
+#                 # there is at least one in the input
+#                 numWays = 1
+#             prevOneIdx = i
+#     return numWays
+
+# #driver code
+
+# arr = '10101'
+# print ("num ways:", countWays(arr, len(arr) ) )
