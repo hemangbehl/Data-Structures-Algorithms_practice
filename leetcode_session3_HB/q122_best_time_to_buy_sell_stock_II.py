31ab017c64f313fc47dc61c95bdacb30b7514ed7
Hemang Behl <44247931+hemangbehl@users.noreply.github.com>
2020-03-07 15:37:24 -0800

    .

---
+++ leetcode_session3_HB/q122_best_time_to_buy_sell_stock_II.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        n = len(prices)
+        
+        if n <= 1:
+            return 0
+        
+        prev = prices[0]
+        profit = 0
+        
+        for i in range(1, n):
+            if prev < prices[i]:
+                profit += ( prices[i] - prev )
+                prev = prices[i]
+            else:
+                prev = prices[i]
+        
+        return profit
