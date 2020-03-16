498e56069cc79276a32ae2d912af78909bd275fd
Hemang Behl <44247931+hemangbehl@users.noreply.github.com>
2020-03-16 14:17:14 -0700

    removed redundant variables

--- leetcode_session3_HB/q994_Rotting Oranges.py
+++ leetcode_session3_HB/q994_Rotting Oranges.py
@@ -5,10 +5,10 @@ class Solution:
         
         #0 no orange
         #1 orange, 2: rotten
-        visited = set()
         queue = []
         minute = 0
         ans = 0
+        fresh_cnt = 0
         
         #loop through each point in grid and check for rotten oranges
         for i in range(rows):
@@ -16,49 +16,34 @@ class Solution:
                 if grid[i][j] == 2:
                     #rotten
                     queue.append( [0, i, j] )
+                if grid[i][j] == 1: #fresh oranges
+                    fresh_cnt += 1
         
         while queue:
-            pair = queue.pop(0) #dequeue
-            minute = pair[0]
-            i = pair[1]
-            j = pair[2]
+            minute, i, j = queue.pop(0) #dequeue
             
             #check all 4 sides and add oranges to queue
-            minute = self.rot(grid, minute, i, j, queue, visited, ans)
+            flag = 0
+            
+            #check all 4 sides
+            for x, y in [(i,j+1),(i,j-1),(i+1,j),(i-1,j)]:
+                if x>=0 and y>=0 and x<len(grid) and y<len(grid[0]) and grid[x][y] == 1:
+                    #rot the side
+                    grid[x][y] = 2 #orange is now rotten
+                    fresh_cnt -= 1 #one less fresh orange
+                    #add to queue to further check its sides
+                    queue.append( [minute+1, x, y] )
+                    flag = 1
+            
+            if flag == 1:
+                #made orange rot
+                minute += 1
             
             ans = max(ans, minute)
         
         
-        #check for remaining fresh oranges
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == 1:
-                    #fresh orange detected
-                    return -1 
+        if fresh_cnt != 0:
+            return -1
+        else:
+            return ans
         
-        return ans
-    
-    def rot(self, grid, minute, i, j, queue, visited, ans):
-        #rot() is called on verified rotten oranges
-        visited.add( (i, j) )
-        flag = 0
-        
-        #check all 4 sides
-        for direc in [(0,1),(0,-1),(1,0),(-1,0)]:
-            x = i + direc[0]
-            y = j + direc[1]
-            
-            if x>=0 and y>=0 and x<len(grid) and y<len(grid[0]) and (x, y) not in visited and grid[x][y] == 1:
-                #rot the side
-                grid[x][y] = 2
-                #add to queue to further check its sides
-                queue.append( [minute+1, x, y] )
-                flag = 1
-        
-        if flag == 1:
-            #made orange rot
-            minute += 1
-        
-        return minute
-        
-            
