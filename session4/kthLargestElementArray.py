7393f441b5334a82b9c9c3073c8d1b07d0dd5c7d
Hemang Behl <44247931+hemangbehl@users.noreply.github.com>
2020-05-31 19:52:14 -0700

    kth largest leetcode215

---
+++ session4/kthLargestElementArray.py
@@ -0,0 +1,23 @@
+import heapq
+def k_largest(arr, k):
+    if len(arr) == 0:
+        return
+    if len(arr) == 1:
+        return arr[0]
+    
+    minheap = []
+
+    for ele in arr:
+        heapq.heappush(minheap, ele)
+
+        #maintain heap size
+        if len(minheap) > k:
+            heapq.heappop(minheap)
+    
+    #top most ele in minheap is kth largest
+    return minheap[0]
+
+arr = [12, 14, 9, 50, 61, 41]
+print(arr)
+k=2
+print( "kth largest ", k, "=", k_largest(arr,k ) )
